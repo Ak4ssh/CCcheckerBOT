2f455145131e1d2e8f753dcb76a19c37dc770269
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-08-21 15:05:59 +0530

    Update gateinfo.py

--- mills/plugins/checkers/utils/gateinfo.py
+++ mills/plugins/checkers/utils/gateinfo.py
@@ -17,24 +17,24 @@ def get_gate_info(cmd_name: str = None,is_shopify = False):
                 return
             if user['type'] == "P":
                 if user['expire_days'] < time.time():
-                    await m.sod("Your Premium Plan has expired. Please contact @r0ld3x for renewing your plan.", time = 5)
+                    await m.sod("Your Premium Plan has expired. Please contact @TheVenomXD for renewing your plan.", time = 5)
                     await mdb.update_one('users',{'_id':m.sender_id},{'plan':"F",'role': "Free"})
                     return
                 # elif user['expire_days'] - time.time() < 3600:
-                #     await m.sod("Your Premium Plan will expire in 1 hour. Please contact @r0ld3x for renewing your plan.", time = 5)
+                #     await m.sod("Your Premium Plan will expire in 1 hour. Please contact @TheVenomXD for renewing your plan.", time = 5)
                 #     return
             else:
                 if m.is_private:
-                    await m.sod("you dont have to use this command in private chat. talk with @r0ld3x for access.", time = 5)
+                    await m.sod("you dont have to use this command in private chat. talk with @TheVenomXD for access.", time = 5)
                     return
                 all_chats = await m.adb.get_key(f'approved_{str(m.chat_id)}')
                 if not m.is_private and not all_chats and m.sender_id not in ADMINS:
-                    await m.sod("This chat is unauthorized to use this bot. Please contact @r0ld3x for more info.", time = 5)
+                    await m.sod("This chat is unauthorized to use this bot. Please contact @TheVenomXD for more info.", time = 5)
                     return
             if is_shopify:
                 if 'shopify_apis' in user:
                     if len(user['shopify_apis']) < 1:
-                        return await m.sod("You have 0 Shopify APIs. Please contact @r0ld3x for more info.", time = 5)
+                        return await m.sod("You have 0 Shopify APIs. Please contact @TheVenomXD for more info.", time = 5)
                     api_name = m.pattern_match.group(1).split('|',maxsplit=1)[0].strip()
                     if not api_name.startswith('api') or not api_name in user['shopify_apis'].keys():
                         return await m.sod("Please use valid api names.", time = 5)
